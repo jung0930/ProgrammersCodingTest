3eb711471fff85451cbf9d30063e8a5a927302b0
jung9030 <wjddlqkqh57@naver.com>
2021-01-11 16:05:14 +0900

    모의고사 리펙토링

--- src/main/java/programmers/jung/lv1/모의고사/MathTest.java
+++ src/main/java/programmers/jung/lv1/모의고사/MathTest.java
@@ -13,19 +13,26 @@ public class MathTest {
 	
     public static int[] solution(int[] answers) {
         int[] answer = {};
-        int[] person1 = {1,2,3,4,5}; //이만큼씩 반복
-        int[] person2 = {2,1,2,3,2,4,2,5};
-        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+        int answersLength = answers.length;
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int person1Length = person1.length;
+        int person2Length = person2.length;
+        int person3Length = person3.length;
+        
         int answer1=0, answer2 =0, answer3 =0;
         
-        for(int i =0; i<answers.length; i++) {
-            if(person1[i%person1.length] == answers[i]) answer1++;
-            if(person2[i%person2.length] == answers[i]) answer2++;
-            if(person3[i%person3.length] == answers[i]) answer3++;
+        for(int i =0; i<answersLength; i++) {
+            if(person1[i%person1Length] == answers[i]) answer1++;
+            if(person2[i%person2Length] == answers[i]) answer2++;
+            if(person3[i%person3Length] == answers[i]) answer3++;
         }
-        int max = Math.max(Math.max(answer1, answer2),answer3); // max값 구하기
+        
+        int max = Math.max(Math.max(answer1, answer2),answer3);
         ArrayList<Integer> list = new ArrayList<Integer>();
-        if(max==answer1) list.add(1); //max값이랑 같으면 넣는다.
+        
+        if(max==answer1) list.add(1);
         if(max==answer2) list.add(2);
         if(max==answer3) list.add(3);
         
