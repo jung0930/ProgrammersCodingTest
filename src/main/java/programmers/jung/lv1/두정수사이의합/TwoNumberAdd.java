6037abda26e64170519d20001e0ff380cde8f010
jungGK <wjddlqkqh57@naver.com>
2021-01-16 21:24:09 +0900

    두 정수 사이의 합 - 등차수열

--- src/main/java/programmers/jung/lv1/두정수사이의합/TwoNumberAdd.java
+++ src/main/java/programmers/jung/lv1/두정수사이의합/TwoNumberAdd.java
@@ -33,5 +33,16 @@ public class TwoNumberAdd {
         
         return answer;
     }
+	
+	
+	/* 등차수열 이용
+    public static long solution(int a, int b) {
+        return sumAtoB(Math.min(a, b), Math.max(b, a));
+    }
+
+    private long sumAtoB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
+    }
+	 */
 
 }
