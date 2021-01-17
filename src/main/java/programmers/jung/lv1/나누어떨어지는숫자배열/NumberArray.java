0e0619e8304c0dceb254fc543de1d0385e9b2903
jungGK <wjddlqkqh57@naver.com>
2021-01-17 21:19:47 +0900

    나누어 떨어지는 숫자 배열

---
+++ src/main/java/programmers/jung/lv1/나누어떨어지는숫자배열/NumberArray.java
@@ -0,0 +1,40 @@
+package main.java.programmers.jung.lv1.나누어떨어지는숫자배열;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NumberArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub [5, 9, 7, 10]	5
+		int[] arr = {5, 9, 7, 10};
+		int divisor = 5;
+		
+		solution(arr, divisor);
+
+	}
+	
+	public static int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]%divisor==0) a1.add(arr[i]);
+        }
+        
+        if(a1.isEmpty()) {
+            a1.add(-1);
+        }
+        
+        answer = new int[a1.size()];
+        
+        for(int i=0; i<a1.size(); i++) {
+            answer[i] = a1.get(i);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+	}
+    
+}
