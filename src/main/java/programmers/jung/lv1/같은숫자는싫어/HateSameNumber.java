37072caeb8df246a0355468fbcc69c5676bfc6cd
jung9030 <wjddlqkqh57@naver.com>
2021-01-18 10:30:45 +0900

    같은 숫자는 싫어

---
+++ src/main/java/programmers/jung/lv1/같은숫자는싫어/HateSameNumber.java
@@ -0,0 +1,27 @@
+package main.java.programmers.jung.lv1.같은숫자는싫어;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12906
+public class HateSameNumber {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 3, 3, 0, 1, 1};
+		solution(arr);
+	}
+	
+	public static int[] solution(int[] arr) {
+		Set<Integer> set = new HashSet<Integer> ();
+		int arrLength = arr.length;
+		
+		for(int i = 0; i < arrLength; i++) {
+			
+			
+		}
+		
+		
+		return null;
+	}
+
+}
