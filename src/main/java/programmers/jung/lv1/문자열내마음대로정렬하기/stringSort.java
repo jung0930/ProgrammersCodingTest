40b830c94b2e434db6b0a95e625ca59d4d6fbe98
jung9030 <wjddlqkqh57@naver.com>
2021-01-27 10:36:32 +0900

    문자열 내 마음대로 정렬하기 수정

--- src/main/java/programmers/jung/lv1/문자열내마음대로정렬하기/stringSort.java
+++ src/main/java/programmers/jung/lv1/문자열내마음대로정렬하기/stringSort.java
@@ -1,6 +1,7 @@
 package main.java.programmers.jung.lv1.문자열내마음대로정렬하기;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12915
@@ -16,21 +17,16 @@ public class stringSort {
 
 	
 	public static String[] solution(String[] strings, int n) {
-		int stringsLength = strings.length;
-		ArrayList<String> list = new ArrayList<String> ();
-		
-		for(int i = 0; i < stringsLength; i++) {
-			list.add("" +strings[i].substring(n, n+1) + strings[i]);
+		for(int i = 0; i < strings.length; i++) {
+			strings[i] = strings[i].charAt(n) + strings[i];
 		}
 		
-		Collections.sort(list);
-		int listSize = list.size();
-		String[] answers = new String[listSize];
+		Arrays.sort(strings);
 		
-		for(int i = 0; i < listSize; i++) {
-			answers[i] = list.get(i).substring(1);
+		for(int i = 0; i < strings.length; i++) {
+			strings[i] = strings[i].substring(1);
 		}
 		
-		return answers;
+		return strings;
 	}
 }
