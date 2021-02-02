d84a402e181762f7b237d42f4a5fa4693c627966
jungGK <wjddlqkqh57@naver.com>
2021-02-02 21:39:39 +0900

    제일 작은 수 제거하기

--- src/main/java/programmers/jung/lv1/제일작은수제거하기/Solution.java
+++ src/main/java/programmers/jung/lv1/제일작은수제거하기/Solution.java
@@ -33,5 +33,27 @@ public class Solution {
         
         return list.toArray(new Integer[list.size()]);
     }
-	
+    
+    /*
+	 if(arr.length == 1){
+          int[] answer = {-1};
+          return answer;
+      }
+
+      int[] answer = new int[arr.length-1];
+      int minIndex=0;
+
+      for(int i=0;i<arr.length;i++){
+          if(arr[minIndex]>arr[i]){
+              minIndex = i;
+          }
+      }
+      for(int i=minIndex+1;i<arr.length;i++){
+          arr[i-1] = arr[i];
+      }
+      for(int i=0;i<answer.length;i++){
+          answer[i] = arr[i];
+      }
+      return answer;
+	*/
 }
