79fd758108d51cbb9e8f942b2516672f6cc7592f
jung9030 <wjddlqkqh57@naver.com>
2021-02-05 10:54:27 +0900

    같은 숫자는 싫어. 다른 풀이

--- src/main/java/programmers/jung/lv1/같은숫자는싫어/HateSameNumber.java
+++ src/main/java/programmers/jung/lv1/같은숫자는싫어/HateSameNumber.java
@@ -28,6 +28,21 @@ public class HateSameNumber {
 		}
 		
 		return answers;
+		
+		/*
+		ArrayList<Integer> tempList = new ArrayList<Integer>();
+        int preNum = 10;
+        for(int num : arr) {
+            if(preNum != num)
+                tempList.add(num);
+            preNum = num;
+        }       
+        int[] answer = new int[tempList.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = tempList.get(i).intValue();
+        }
+        return answer;
+		 */
 	}
 
 }
