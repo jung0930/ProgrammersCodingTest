feb8e4042607e9797ba47693db2c00da3609bc74
jungGK <wjddlqkqh57@naver.com>
2021-02-12 23:52:09 +0900

    콜라츠추측 수정

--- src/main/java/programmers/jung/lv1/콜라츠추측/Solution.java
+++ src/main/java/programmers/jung/lv1/콜라츠추측/Solution.java
@@ -27,5 +27,14 @@ public class Solution {
     	}
     	
     	return count;
+    	
+    	/*
+    	long n = (long)num;
+	    for(int i =0; i<500; i++){      
+	      if(n==1) return i;
+	      n = (n%2==0) ? n/2 : n*3+1;            
+	    }
+	    return -1;
+    	*/
     }
 }
