b8f1a9c7197ce366dbdb05a8a6e462791c17241c
jungGK <wjddlqkqh57@naver.com>
2021-02-14 22:47:08 +0900

    행렬의 덧셈

--- src/main/java/programmers/jung/lv1/행렬의덧셈/Solution.java
+++ src/main/java/programmers/jung/lv1/행렬의덧셈/Solution.java
@@ -22,5 +22,18 @@ public class Solution {
     	}
     	
         return answer;
+        
+        /*
+        int row = Math.max(arr1.length, arr2.length);
+        int col = Math.max(arr1[0].length, arr2[0].length);
+        int[][] answer = new int[row][col];
+        for(int i=0; i<row ; i++){
+          for(int j=0; j<col; j++){
+            answer[i][j] = arr1[i][j] + arr2[i][j];
+          }
+        }
+
+        return answer;
+        */
     }
 }
