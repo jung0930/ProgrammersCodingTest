f9a3803c5bb0a53062fbbe0ead583194635712c0
jung9030 <wjddlqkqh57@naver.com>
2021-02-17 11:12:01 +0900

    모의고사 수정

--- src/main/java/programmers/jung/lv1/모의고사/MathTest.java
+++ src/main/java/programmers/jung/lv1/모의고사/MathTest.java
@@ -36,8 +36,6 @@ public class MathTest {
     	if(max == count2) list.add(2);
     	if(max == count3) list.add(3);
     	
-    	// return list.stream().mapToInt(i->i.intValue()).toArray();
-    	
     	int[] answer = new int[list.size()];
     	int answerLength = answer.length;
     	
@@ -46,6 +44,35 @@ public class MathTest {
     	}
     	
         return answer;
+        
+        /*
+        int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
+            }
+        }
+
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
+
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
+        return answer;
+        */
+        
+        // return list.stream().mapToInt(i->i.intValue()).toArray();
+        
     }
     
     
