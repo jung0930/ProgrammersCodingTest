6e87835190eb230c8ba12be99bce292aecde6534
jungGK <wjddlqkqh57@naver.com>
2021-02-18 21:15:49 +0900

    최대공약수와 최소공배수

--- src/main/java/programmers/jung/lv1/최대공약수와최소공배수/Solution.java
+++ src/main/java/programmers/jung/lv1/최대공약수와최소공배수/Solution.java
@@ -29,5 +29,20 @@ public class Solution {
     	
         return answer;
     }
-	
+
+    
+    /* 재귀함수
+    public static int[] solution(int a, int b) {
+        int[] answer = new int[2];
+        answer[0] = gcd(a,b);
+        answer[1] = (a*b)/answer[0];
+        return answer;
+    }
+
+	public static int gcd(int p, int q) {
+		if (q == 0) return p;
+    	return gcd(q, p%q);
+	}
+    */
+    
 }
