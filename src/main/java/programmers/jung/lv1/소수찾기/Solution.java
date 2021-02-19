2c06ca88b2abe00ed043106015a6548ce210ed61
jung9030 <wjddlqkqh57@naver.com>
2021-02-19 16:32:54 +0900

    소수찾기 수정

--- src/main/java/programmers/jung/lv1/소수찾기/Solution.java
+++ src/main/java/programmers/jung/lv1/소수찾기/Solution.java
@@ -35,5 +35,20 @@ public class Solution {
         }
         
         return answer;
+        
+        /*
+	 	int result = 0;
+	    for(int i=2; i<=n; i++){
+	        for(int j=2; j<=i; j++){
+	        if(j == i){
+	            ++result;
+	        } else if(i%j == 0){
+	            break;
+	        }
+	      }
+	    }
+	
+		return result;
+        */
     }
 }
