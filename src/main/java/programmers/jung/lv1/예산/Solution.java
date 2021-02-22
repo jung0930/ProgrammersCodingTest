d588e75867950f284c6f8b21b0360d8f55e6baae
jungGK <wjddlqkqh57@naver.com>
2021-02-22 21:44:17 +0900

    예산 추가

--- src/main/java/programmers/jung/lv1/예산/Solution.java
+++ src/main/java/programmers/jung/lv1/예산/Solution.java
@@ -27,6 +27,22 @@ public class Solution {
           answer++;
         }
         return answer;
+        
+        /*
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget < 0) break;
+
+            answer++;
+        }
+
+        return answer;
+        */
     }
     
     
